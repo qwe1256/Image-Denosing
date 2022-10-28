bbdb31371ea6ab32cb6c582a9dfa6be3dbfc29dd
qwe1256 <dih4@uci.edu>
2022-10-28 14:04:36 -0500

    clean code

--- train.py
+++ train.py
@@ -10,14 +10,12 @@ import torch.optim as optim
 import torchvision.transforms.functional as F
 import torchvision.utils as utils
 import torchvision.transforms as transforms
-from torch.autograd import Variable
 from torch.utils.data import DataLoader
 from tensorboardX import SummaryWriter
 from math import ceil
 from models import DnCNN
-from dataset import prepare_data, Dataset
+from dataset import Dataset
 from utils import *
-from matplotlib import pyplot as plt
 
 
 environ["CUDA_DEVICE_ORDER"] = "PCI_BUS_ID"
@@ -98,6 +96,7 @@ def main(cfg):
             psnr_train = batch_PSNR(out_train, img_train, 1.)
             print("[epoch %d][%d/%d] loss: %.4f PSNR_train: %.4f" %
                 (epoch+1, i+1, len(loader_train), loss.item(), psnr_train))
+            #log.info("Info level message")
             if step % 10 == 0:
                 # Log the scalar values
                 writer.add_scalar('loss', loss.item(), step)
